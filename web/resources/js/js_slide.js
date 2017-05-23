2221b04a71c0837b642df39e2b84f46606a295e8
giosan777 <giosan777@gmail.com>
2017-05-23 19:27:18 +0400

    dasndliasmk

--- web/resources/js/js_slide.js
+++ web/resources/js/js_slide.js
@@ -1,10 +1,39 @@
-alert("me var");
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+all_images = ["#{resource['images/img_slide/cxovelebi.png']}",
+    "#{resource['images/img_slide/avto.png']}",
+    "#{resource['images/img_slide/fentezi.png']}",
+    "#{resource['images/img_slide/kino.png']}",
+    "#{resource['images/img_slide/musika.png']}",
+    "#{resource['images/img_slide/smailebi.png']}",
+    "#{resource['images/img_slide/siyvaruli.png']}",
+    "#{resource['images/img_slide/tamashebi.png']}"
+];
+var ImgNum = 0;
+var ImgLength = all_images.length - 1;
+var delay = 2500;
+var lock = false;
+var run;
 
-function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+function chgImg(direction) {
+    if (document.images) {
+        ImgNum = ImgNum + direction;
+        if (ImgNum > ImgLength) {
+            ImgNum = 0;
+        }
+        if (ImgNum &lt; 0) {
+            ImgNum = ImgLength;
+        }
+        setTimeout("document.getElementsByName('slide_show').src= all_images[ImgNum]");
+        // document.getElementsByName().src = all_images[ImgNum];
+    }
+}
+
+function auto() {
+    if (lock == true) {
+        lock = false;
+        window.clearInterval(run);
+    }
+    else if (lock == false) {
+        lock = true;
+        run = setInterval("chgImg(1)", delay);
+    }
 }
